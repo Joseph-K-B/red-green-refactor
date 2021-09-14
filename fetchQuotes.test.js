52d6e02dcf01880ba6043169115612b97130649c
Joseph-K-B <303josephb@gmail.com>
2021-09-13 20:31:41 -0700

    I like the request response naming convention better

--- fetchQuotes.test.js
+++ fetchQuotes.test.js
@@ -2,9 +2,9 @@ import fetchQuotes from './fetchQuotes';
 
 describe('fetchQuotes', () => {
   it('it should return a quote including name, text, & image from futurama API', async () => {
+    const req = await fetchQuotes();
     const res = expect.any(String);
-    const quotes = await fetchQuotes();
-    expect(quotes).toEqual({
+    expect(req).toEqual({
       character: res,
       image: res,
       quote: res,
