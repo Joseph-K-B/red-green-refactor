f11b9c1f48b50c5fd11e32fce03e3519b6941876
Joseph-K-B <303josephb@gmail.com>
2021-09-13 17:29:37 -0700

    capitalizeAndFilter passing using map & filter methods

--- capitalizeAndFilter.js
+++ capitalizeAndFilter.js
@@ -1,11 +1,9 @@
 const capitalizeAndFilter = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-    if (arr[i].charAt(0) === 'F') {
-      arr.splice(i);
-    }
-  }
-  return arr;
+  const string = arr.map((str) => 
+    str.charAt(0).toUpperCase() + str.slice(1)   
+  );
+  const newArr = string.filter(item => !item.startsWith('F'));
+  return newArr;
 };
 
 export default capitalizeAndFilter;
