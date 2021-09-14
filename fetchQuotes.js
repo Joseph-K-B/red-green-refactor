fea6ef02297433f1a5445722617f6ed5ce7b92c8
Joseph-K-B <303josephb@gmail.com>
2021-09-13 20:24:52 -0700

    destructured and accounted for random quotes across all characters

--- fetchQuotes.js
+++ fetchQuotes.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 
 const fetchQuotes = async () => {
-  const url = 'https://futuramaapi.herokuapp.com/api/characters/Fry';
-  const res = await fetch(url);
-  const body = await res.json();
-  return body[0];
+  const url = 'https://futuramaapi.herokuapp.com/api/quotes';
+  const req = await fetch(url);
+  const body = await req.json();
+  return { character:body[0].character, quote:body[0].quote, image:body[0].image };
 };
 
 export default fetchQuotes;
