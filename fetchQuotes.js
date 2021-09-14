ae0a08da67ee5751994a644c8ea212238a094487
Joseph-K-B <303josephb@gmail.com>
2021-09-13 20:16:11 -0700

    fetchQuotes passing dynamically, still needs a little work

--- fetchQuotes.js
+++ fetchQuotes.js
@@ -1,12 +1,9 @@
 import fetch from 'node-fetch';
 
 const fetchQuotes = async () => {
-  const url = 'https://futuramaapi.herokuapp.com/api/characters/fry';
+  const url = 'https://futuramaapi.herokuapp.com/api/characters/Fry';
   const res = await fetch(url);
   const body = await res.json();
-
-  //   return body.map((item) => item.character);
-  console.log(body[0]);
   return body[0];
 };
 
